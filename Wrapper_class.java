91a9707d203bdeccb9ce6ce531be92142e28a7cd
trashar18 <80939436+trashar18@users.noreply.github.com>
2021-12-02 23:19:38 +0530

    Create Wrapper_class.java

---
+++ Wrapper_class.java
@@ -0,0 +1,22 @@
+public class Wrapper_class {
+    public static void main(String[] args) {
+//        Integer obj = 20;
+//        Integer obj = Integer.valueOf(45);
+//        Integer obj1 = Integer.valueOf(45);       //boxing
+////        System.out.println(obj.equals(obj1));
+//        System.out.println(obj==obj1);
+
+//        Wrapper_class obj2=new Wrapper_class();
+//        Wrapper_class obj3= new Wrapper_class();     //obj equal return true
+//        System.out.println(obj2==obj3);
+
+        Integer obj =Integer.valueOf(60);        //boxing
+        Integer obj1 =Integer.valueOf(60);
+//        int obj3=obj;                             unboxing
+
+
+
+
+
+    }
+}
