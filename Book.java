073cafea6e8b626b70d72379294fbd40d4ae15d6
trashar18 <80939436+trashar18@users.noreply.github.com>
2021-12-02 23:26:46 +0530

    Create Book.java

---
+++ Book.java
@@ -0,0 +1,9 @@
+public class class{
+  String name;
+  Static String author="H.C Veerma";
+  double price;
+  int qtyInStock;
+  class(String name,double price,int qtyInStock){
+  this.name=name;
+  this.price=price;
+  this.qtyInStock=qtyInStock;
